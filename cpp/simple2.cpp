d3c55a444879d430209ced42452f223c8b887d6c
swugit2 <swu.gitacct2@gmail.com>
2016-08-22 17:07:05 -0400

    added a simple2.cpp in prod1.3_GP

---
+++ cpp/simple2.cpp
@@ -0,0 +1,33 @@
+/*
+ * Simple C++ program
+ */
+// added a comment line
+#include <iostream>
+#include <sstream>
+using namespace std;
+
+class small
+{
+public:
+    int x;
+};
+
+class big : public small
+{
+public:
+    double y;
+};
+
+int main()
+{
+    small var1;
+    big var2;
+
+    var1.x = 1;
+    var2.y = double (var1.x) + 2.0;
+
+    cout << "small.x = " << var1.x << endl;
+    cout << "big.y = "   << var2.y << endl;
+
+    return (0);
+}
